10d3ff91cc327850a45b7859ecff6877e8f248bb
Christopher Rodriguez <mrmcpowned@users.noreply.github.com>
2026-04-19 00:36:08 -0700

    chore: update base path for renamed repo (shellos)

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/shellhacks-x/',
+  base: '/shellos/',
   assetsInclude: ['**/*.frag', '**/*.vert'],
   server: {
     host: true,
